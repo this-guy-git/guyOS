11f3415e66f0bc3a92e7d52c21669fa73a85a75e
this guy <witherx16@gmail.com>
2025-12-05 16:58:54 -0700

    [SHELL] Add Headers with version vars and ascii art header

--- include/ver.h
+++ include/ver.h
@@ -1,1 +1,5 @@
-char version[] = "v0.0.4";
+static const char version[] = "v7.0.5";
+static const char shellver[] = "v0.0.8";
+static const char kernelver[] = "v0.0.5";
+static const char build_yr[] = "2025";
+static const char gs_ver[] = "v0.0.3";
